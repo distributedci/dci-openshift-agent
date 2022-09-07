7a4e093e00fe8833316c5f27c2fd87639cf3c80b
Tony Garcia <tonyg@redhat.com>
2022-09-07 16:11:16 -0500

    Add filter to sort versions
    - This will be helpful to sort correctly a list of versions in EUS and
      other places
    
    Change-Id: I61439e7fa55cb4e165408cc0391ba6cdda31dc6b

---
+++ filter_plugins/version_sort.py
@@ -0,0 +1,10 @@
+from packaging.version import Version
+
+
+def version_sort(l):
+    return sorted(l, key=Version)
+
+
+class FilterModule(object):
+    def filters(self):
+        return {"version_sort": version_sort}
